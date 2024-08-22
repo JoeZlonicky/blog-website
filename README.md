3e283f36b3b56c01ae33325846d493c633dc120f
JoeZlonicky <joe.zlonicky@gmail.com>
2024-08-22 16:43:29 -0700

    Fetch posts

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ A Vue.js blog website.
 
 ## Running
 
-1. Install packages with `pnpm install`
-2. Run a local dev server with `pnpm dev`
+1. Create a `.env` file that follows the `.env.example` template.
+2. Install packages with `pnpm install`
+3. Run a local dev server with `pnpm dev`
 
 ## Building
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,37 @@
-<script setup></script>
+<script setup>
+import { getPosts } from '@/api/getPosts.js';
+import { onMounted, ref } from 'vue';
+
+const posts = ref([]);
+const isFetching = ref(true);
+const wasFetchSuccessfull = ref(undefined);
+
+async function fetchPosts() {
+  try {
+    posts.value = await getPosts();
+    wasFetchSuccessfull.value = true;
+  } catch (err) {
+    wasFetchSuccessfull.value = false;
+  }
+
+  isFetching.value = false;
+}
+
+onMounted(async () => {
+  await fetchPosts();
+});
+</script>
 
 <template>
   <main>
     <h1 class="text-3xl">Welcome to the blog!</h1>
+    <p v-if="isFetching">Fetching posts...</p>
+    <p v-else-if="!wasFetchSuccessfull">Failed to fetch posts.</p>
+    <p v-else-if="posts.length === 0">No posts.</p>
+    <div v-else>
+      <div v-for="post in posts" :key="post.id">
+        {{ post.title }} by {{ post.author.username }}
+      </div>
+    </div>
   </main>
 </template>
